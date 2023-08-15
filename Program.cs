626b2b352e5696d19e45536ae3972ab652218516
RSL <RSL@LAPTOP-VQ4R2BLH>
2023-08-15 12:10:19 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿//Value pattern
+
+string color = "r";
+
+var result = color switch
+{
+    "r" => "red",
+    "b" => "blue",
+    "g" => "green",
+    _ => "Unknown"
+};
+
+Console.WriteLine(result);
+
+//Type pattern
