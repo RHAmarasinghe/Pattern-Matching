a1ce309979a90c1e6d6a0e139cafe9e6910fd871
RHAmarasinghe <RSL@LAPTOP-VQ4R2BLH>
2023-08-15 12:33:19 +0530

    Adding type pattern

--- Program.cs
+++ Program.cs
@@ -13,3 +13,26 @@ var result = color switch
 Console.WriteLine(result);
 
 //Type pattern
+
+int age = 20;
+string name = "Rashmi";
+var numbers = new int[] { 1, 2, 3, 4, 5 };
+double salary = 20000.00;
+List<string> cities = new List<string> {"Colombo", "Kandy", "Galle"};
+
+object CheckType(object obj) => obj switch
+{
+    int => "Integer Type",
+    string => "String Type",
+    List<string> => "Generic string List",
+    Array => "Array Type",
+    _ => "Unknown"
+};
+
+Console.WriteLine(CheckType(age));
+Console.WriteLine(CheckType(name));
+Console.WriteLine(CheckType(numbers));
+Console.WriteLine(CheckType(salary));
+Console.WriteLine(CheckType(cities));
+
+Console.ReadLine();
